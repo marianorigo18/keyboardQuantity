61523fc35fa52f0a5adf3004969bb36fcb2f29a9
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-22 11:49:33 -0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+const buttonAddQ = document.querySelector('#buttonAddQy');
+
+buttons.forEach(element => {
+    element.onclick=()=>{
+        if(element.id=="backspace"){
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0,string.length-1)
+        }else{
+            display.innerText += element.id     
+        }
+    }
+})
